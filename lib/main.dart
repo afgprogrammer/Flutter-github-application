b329e3b51d418cda572af0b71c1775767e598239
Mohammad Rahmani <rto1680@gmail.com>
2019-09-30 22:11:12 +0430

    Make main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:github_following/Pages/FollowingPage.dart';
+import 'package:github_following/Providers/UserProvider.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+  ChangeNotifierProvider<UserProvider>(
+    builder: (context) => UserProvider(),
+    child: MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  )
+);
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _StateHomePage createState() => _StateHomePage();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class _StateHomePage extends State<HomePage> {
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  TextEditingController _controller = TextEditingController();
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _getUser() {
+    if (_controller.text == '') {
+      Provider.of<UserProvider>(context).setMessage('Please inter your username');
+    } else {
+      Provider.of<UserProvider>(context).fetchUser(_controller.text).then((value) {
+        if (value) {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => FollowingPage()));
+        }
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 100,),
+              Container(
+                width: 80,
+                height: 80,
+                child: CircleAvatar(
+                  backgroundColor: Colors.transparent,
+                  backgroundImage: NetworkImage('https://icon-library.net/images/github-icon-png/github-icon-png-29.jpg'),
+
+                ),
+
+              ),
+              SizedBox(height: 30,),
+              Text("Github", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
+              SizedBox(height: 150,),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white.withOpacity(.1)
+                ),
+                child: TextField(
+                  onChanged: (value) {
+                    Provider.of<UserProvider>(context).setMessage(null);
+                  },
+                  controller: _controller,
+                  enabled: !Provider.of<UserProvider>(context).isLoading(),
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    errorText: Provider.of<UserProvider>(context).getMessage(),
+                    border: InputBorder.none,
+                    hintText: "Github username",
+                    hintStyle: TextStyle(color: Colors.grey)
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              MaterialButton(
+                padding: EdgeInsets.all(20),
+                color: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Align(
+                  child: 
+                    Provider.of<UserProvider>(context).isLoading() ?
+                    CircularProgressIndicator(backgroundColor: Colors.white, strokeWidth: 2,) :
+                    Text('Get Your Following Now', style: TextStyle(color: Colors.white),),
+                ), onPressed: () {
+                  _getUser();
+                },
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
