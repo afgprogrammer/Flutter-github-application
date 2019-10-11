859214e05f392e5e24581b1f9aaa13b35c25b305
Mohammad Rahmani <rto1680@gmail.com>
2019-10-11 20:52:38 +0430

    Fix issue #1

--- lib/main.dart
+++ lib/main.dart
@@ -37,64 +37,67 @@ class _StateHomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.black,
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 100,),
-              Container(
-                width: 80,
-                height: 80,
-                child: CircleAvatar(
-                  backgroundColor: Colors.transparent,
-                  backgroundImage: NetworkImage('https://icon-library.net/images/github-icon-png/github-icon-png-29.jpg'),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          color: Colors.black,
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 100,),
+                Container(
+                  width: 80,
+                  height: 80,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    backgroundImage: NetworkImage('https://icon-library.net/images/github-icon-png/github-icon-png-29.jpg'),
+
+                  ),
 
                 ),
-
-              ),
-              SizedBox(height: 30,),
-              Text("Github", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
-              SizedBox(height: 150,),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white.withOpacity(.1)
-                ),
-                child: TextField(
-                  onChanged: (value) {
-                    Provider.of<UserProvider>(context).setMessage(null);
-                  },
-                  controller: _controller,
-                  enabled: !Provider.of<UserProvider>(context).isLoading(),
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                    errorText: Provider.of<UserProvider>(context).getMessage(),
-                    border: InputBorder.none,
-                    hintText: "Github username",
-                    hintStyle: TextStyle(color: Colors.grey)
+                SizedBox(height: 30,),
+                Text("Github", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
+                SizedBox(height: 150,),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white.withOpacity(.1)
+                  ),
+                  child: TextField(
+                    onChanged: (value) {
+                      Provider.of<UserProvider>(context).setMessage(null);
+                    },
+                    controller: _controller,
+                    enabled: !Provider.of<UserProvider>(context).isLoading(),
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      errorText: Provider.of<UserProvider>(context).getMessage(),
+                      border: InputBorder.none,
+                      hintText: "Github username",
+                      hintStyle: TextStyle(color: Colors.grey)
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20,),
-              MaterialButton(
-                padding: EdgeInsets.all(20),
-                color: Colors.blue,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)
-                ),
-                child: Align(
-                  child: 
-                    Provider.of<UserProvider>(context).isLoading() ?
-                    CircularProgressIndicator(backgroundColor: Colors.white, strokeWidth: 2,) :
-                    Text('Get Your Following Now', style: TextStyle(color: Colors.white),),
-                ), onPressed: () {
-                  _getUser();
-                },
-              )
-            ],
+                SizedBox(height: 20,),
+                MaterialButton(
+                  padding: EdgeInsets.all(20),
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Align(
+                    child: 
+                      Provider.of<UserProvider>(context).isLoading() ?
+                      CircularProgressIndicator(backgroundColor: Colors.white, strokeWidth: 2,) :
+                      Text('Get Your Following Now', style: TextStyle(color: Colors.white),),
+                  ), onPressed: () {
+                    _getUser();
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
